1b427ab18abc116243548684b75b269cacc023a5
Ikadev95 <fegughi@gmail.com>
2024-12-16 14:26:54 +0100

    Initial commit

---
+++ src/main/java/com/epicode/spring_w1_d1/PizzaApplication.java
@@ -0,0 +1,13 @@
+package com.epicode.spring_w1_d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epicode/spring_w1_d1/PizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.spring_w1_d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
